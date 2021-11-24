ae32fd49787314642806af3736955631f20ff5d6
siam3371 <siamhossain3371@gmail.com>
2021-11-24 10:39:59 +0600

    my new commit

--- js/app.js
+++ js/app.js
@@ -1,75 +1,37 @@
-// cost function
+//  benifit of extra funciton
+function pruductFunction( price, id){ 
+ const memoryPrudct =   document.getElementById(id);
+ memoryPrudct.innerText = price;
+  } 
+//   total price derection
+
+
+// memory
+ document.getElementById('memory-cost-1').addEventListener('click', function(){
+    //   const MemoryCost = document.getElementById('memory-price').innerText = 20;
+    pruductFunction(0,  'memory-price' )
+    advancedFunction('memory-price');
+   })
+   document.getElementById('memory-cost').addEventListener('click', function(){
+pruductFunction(180,'memory-price')
+    // const memoryCostNew = document.getElementById('memory-price').innerText = 100;
+    advancedFunction('memory-price')
+})
+// storage
+ document.getElementById('without-ssd-cost').addEventListener('click', function() {
+pruductFunction(0, 'storage-price');
+ }) 
+ document.getElementById('low-ssd-cost').addEventListener('click', function(){
+    pruductFunction(100, 'storage-price')
+ });
+ document.getElementById('heigh-ssd-cost').addEventListener('click', function(){
+    pruductFunction(180, 'storage-price');
+ })
+
+ function loadDliveryData(){
+     pruductFunction(0, 'delivery-price');
+ } ;
+ function loadDataDelivery(){
+     pruductFunction(20,  'delivery-price' ) 
+ }
  
-function getPrice(productName, price) {
-    const product = document.getElementById(productName + '-price');
-    product.innerText = parseFloat(price);
-    // const product = document.getElementById
-    updateTotalPrice ();
-    getPomoCode();
-}
-// total price
-function getPriceText(cost) {
-    const productPrice = document.getElementById(cost + '-price');
-    const productPriceText = parseFloat(productPrice.innerText);
-    return productPriceText;
-}
-function updateTotalPrice () {
-    const bestPrice = getPriceText('best');
-    const memoryPrice = getPriceText('memory');
-    const storagePrice = getPriceText('storage');
-    const deliveryPrice = getPriceText('delivery');
-    var totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = bestPrice + memoryPrice + storagePrice + deliveryPrice;
-    return totalPrice.innerText;
-}
-//pomo code
-function getPomoCode() {
-    var fullTotal = document.getElementById('full-total-extra-part');
-    fullTotal.innerText = updateTotalPrice ();
-    var pomoInput = document.getElementById('pomo-input');
-    if (pomoInput.value == 'stevekaku') {
-        fullTotal.innerText = (parseFloat(updateTotalPrice ()) / 100) * 80;
-        pomoInput.value = '';
-    }
-}
-// memory cost
-//without memory cost
-document.getElementById('memory-cost-1').addEventListener('click', function () {
-    getPrice('memory', 0);
-})
-//with memory cost
-document.getElementById('memory-cost').addEventListener('click', function () {
-    getPrice('memory', 180);
-})
-// Storage cost
-// without storage cost
-document.getElementById('without-ssd-cost').addEventListener('click', function () {
-    getPrice('storage', 0);
-})
-// with low storage cost
-document.getElementById('low-ssd-cost').addEventListener('click', function () {
-    getPrice('storage', 100);
-})
-// with heigh storage cost
-document.getElementById('heigh-ssd-cost').addEventListener('click', function () {
-    getPrice('storage', 180);
-})
-// delivery charges
-// without devery cost
-document.getElementById('without-delivery-cost').addEventListener('click', function () {
-    getPrice('delivery', 0);
-})
-// with devery cost
-document.getElementById('delivery-cost').addEventListener('click', function () {
-    getPrice('delivery', 20);
-})
-// pomo code 
-document.getElementById('pomo-btn').addEventListener('click', function () {
-    getPomoCode();
-})
-
-
-
-  
-
-
